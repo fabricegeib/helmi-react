0d544a134fdf63c7ca5096db56be9dc290c6b516
Fabrice Geib <fabricegeib@gmail.com>
2019-08-05 18:37:11 +0200

    first test - no font

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './reset.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi! My name is <span>Helmi Domínguez.</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          I'm a multicultural UI-UX designer currently based in Helsinki.
+        </p>
+        <p>
+          I enjoy solving problems to fulfill user needs.
+        </p>
       </header>
+      <section className="App-section">
+        <p>
+          Do not hesitate top drop me a <a className="App-link" href="mailto:hello@helmi.im" target="_blank" rel="noopener noreferrer">message!</a>
+        </p>
+        <p>More about me</p>
+      </section>
+      <footer className="App-footer">
+        <p>© Helmi Domínguez, 2019</p>
+      </footer>
     </div>
   );
 }
