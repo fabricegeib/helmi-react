2b20552bd8dd5a78c570895bc61034ebffe613bb
Fabrice Geib <fabricegeib@gmail.com>
2022-05-13 15:49:06 +0200

    text update

--- src/App.js
+++ src/App.js
@@ -8,30 +8,30 @@ const name = 'Helmi Domínguez Vanha-aho';
 const year = (new Date().getFullYear());
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hi! My name is <span>{name}</span>,
-        </p>
-        <p>
-          I'm a multicultural UI-UX designer currently based in Helsinki.
-        </p>
-        <p>
-          From early conceptualizations to project implementation, I enjoy solving problems to fulfill user needs.
-        </p>
-      </header>
-      <section className="App-section">
-        <p>
-          Do not hesitate to drop me a <a className="App-link" href="mailto:hello@helmi.im" target="_blank" rel="noopener noreferrer">message!</a>
-        </p>
-        <p><a className="App-link" href="https://linkedin.com/in/helmidv" target="_blank" rel="noopener noreferrer">More about me</a></p>
-      </section>
-      <footer className="App-footer">
-        <p>© {name}, {year}</p>
-      </footer>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					Hi! My name is <span>{name}</span>,
+				</p>
+				<p>
+					I'm a multicultural Product Designer currently based in Helsinki.
+				</p>
+				<p>
+					From early conceptualizations to project implementation, I enjoy solving problems to fulfill user needs.
+				</p>
+			</header>
+			<section className="App-section">
+				<p>
+					Do not hesitate to drop me a <a className="App-link" href="mailto:hello@helmi.im" target="_blank" rel="noopener noreferrer">message!</a>
+				</p>
+				<p><a className="App-link" href="https://linkedin.com/in/helmidv" target="_blank" rel="noopener noreferrer">More about me</a></p>
+			</section>
+			<footer className="App-footer">
+				<p>© {name}, {year}</p>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
